829d6f38f762e6ee8c433f805718af2d666e10aa
carsonxu <459452372@qq.com>
2018-05-28 21:53:52 +0800

    Update sts.php

--- server/sts.php
+++ server/sts.php
@@ -148,6 +148,6 @@ $tempKeys = getTempKeys();
 
 // 返回数据给前端
 header('Content-Type: application/json');
-header('Allow-Control-Allow-Origin: http://127.0.0.1'); // 这里修改允许跨域访问的网站
-header('Allow-Control-Allow-Headers: origin,accept,content-type');
+header('Access-Control-Allow-Origin: http://127.0.0.1'); // 这里修改允许跨域访问的网站
+header('Access-Control-Allow-Headers: origin,accept,content-type');
 echo json_encode($tempKeys);
