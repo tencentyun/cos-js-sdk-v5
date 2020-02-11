52789a0543f0ae859c71104650228a2a475a77f4
carsonxu <g459452372>
2020-02-11 15:48:01 +0800

    fix webpack require error

--- server/sts.php
+++ server/sts.php
@@ -13,7 +13,7 @@ $config = array(
     'bucket' => 'test-1250000000', // 换成你的 bucket
     'region' => 'ap-guangzhou', // 换成 bucket 所在园区
     'durationSeconds' => 1800, // 密钥有效期
-    'allowPrefix' => '*', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
+    'allowPrefix' => '_ALLOW_DIR_/*', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
     // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
     'allowActions' => array (
         // 所有 action 请看文档 https://cloud.tencent.com/document/product/436/31923
