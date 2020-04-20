9c40980195eb5fe8b035429cf7f612319cf1a6c3
carsonxu <459452372@qq.com>
2020-04-20 10:54:15 +0800

    Update sts.php

--- server/sts.php
+++ server/sts.php
@@ -13,8 +13,9 @@ $config = array(
     'bucket' => 'test-1250000000', // 换成你的 bucket
     'region' => 'ap-guangzhou', // 换成 bucket 所在园区
     'durationSeconds' => 1800, // 密钥有效期
-    'allowPrefix' => '_ALLOW_DIR_/*', // 允许操作（上传）的对象前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
-                                      // 请注意当使用 * 时，可能存在安全风险，详情请参阅：https://cloud.tencent.com/document/product/436/40265
+    // 允许操作（上传）的对象前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
+    // 请注意当使用 * 时，可能存在安全风险，详情请参阅：https://cloud.tencent.com/document/product/436/40265
+    'allowPrefix' => '_ALLOW_DIR_/*',
     // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
     'allowActions' => array (
         // 所有 action 请看文档 https://cloud.tencent.com/document/product/436/31923
