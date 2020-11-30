26a1063aa1a6ee4c8adc9137c874cf1e5bb34dee
carsonxu <459452372@qq.com>
2020-11-30 17:02:02 +0800

    增加示例

--- demo/demo.js
+++ demo/demo.js
@@ -679,6 +679,176 @@ function deleteBucketWebsite() {
     });
 }
 
+function putBucketReferer() {
+    cos.putBucketReferer({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region,
+        RefererConfiguration: {
+            Status: 'Enabled',
+            RefererType: 'White-List',
+            DomainList: {
+                Domains: [
+                    '*.qq.com',
+                    '*.qcloud.com',
+                ]
+            },
+            EmptyReferConfiguration: 'Allow',
+        }
+    }, function (err, data) {
+        logger.log(err || data);
+    });
+}
+
+function getBucketReferer() {
+    cos.getBucketReferer({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region
+    },function(err, data){
+        logger.log(err || JSON.stringify(data, null, '    '));
+    });
+}
+
+function putBucketDomain() {
+    cos.putBucketDomain({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region,
+        DomainRule:[{
+            Status: "DISABLED",
+            Name: "www.testDomain1.com",
+            Type: "REST"
+        }, {
+            Status: "DISABLED",
+            Name: "www.testDomain2.com",
+            Type: "WEBSITE"
+        }]
+    },function(err, data){
+        logger.log(err || data);
+    });
+}
+
+function getBucketDomain() {
+    cos.getBucketDomain({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region
+    },function(err, data){
+        logger.log(err || data);
+    });
+}
+
+function deleteBucketDomain() {
+    cos.deleteBucketDomain({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region
+    },function(err, data){
+        logger.log(err || data);
+    });
+}
+
+function putBucketLogging() {
+    var AppId = config.Bucket.substr(config.Bucket.lastIndexOf('-') + 1);
+    cos.putBucketLogging({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region,
+        BucketLoggingStatus: {
+            LoggingEnabled: {
+                TargetBucket: 'bucket-logging-' + AppId,
+                TargetPrefix: 'logging'
+            }
+        }
+    }, function (err, data) {
+        logger.log(err || data);
+    });
+}
+
+function getBucketLogging() {
+    cos.getBucketLogging({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region
+    }, function (err, data) {
+        logger.log(err || data);
+    });
+}
+
+function deleteBucketLogging() {
+    cos.putBucketLogging({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region,
+        BucketLoggingStatus: ''
+    }, function (err, data) {
+        logger.log(err || data);
+    });
+}
+
+function putBucketInventory() {
+    var AppId = config.Bucket.substr(config.Bucket.lastIndexOf('-') + 1);
+    cos.putBucketInventory({
+        Bucket: config.Bucket, // Bucket 格式：test-1250000000
+        Region: config.Region,
+        Id: 'inventory_test',
+        InventoryConfiguration: {
+            Id: 'inventory_test',
+            IsEnabled: 'true',
+            Destination: {
+                COSBucketDestination: {
+                    Format: 'CSV',
+                    AccountId: config.Uin,
+                    Bucket: 'qcs::cos:ap-guangzhou::bucket-logging-' + AppId,
+                    Prefix: 'inventory',
+                    Encryption: {
+                        SSECOS: ''
+                    }
+                }
+            },
+            Schedule: {
+                Frequency: 'Daily'
+            },
+            Filter: {
+                Prefix: 'myPrefix'
+            },
+            IncludedObjectVersions: 'All',
+            OptionalFields: [
+                'Size',
+                'LastModifiedDate',
+                'ETag',
+                'StorageClass',
+                'IsMultipartUploaded',
+                'ReplicationStatus'
+            ]
+        }
+    }, function (err, data) {
+        logger.log(err || data);
+    });
+}
+
+function getBucketInventory() {
+    cos.getBucketInventory({
+        Bucket: config.Bucket,
+        Region: config.Region,
+        Id: 'inventory_test'
+    }, function(err, data) {
+        logger.log(err || JSON.stringify(data));
+    });
+}
+
+function deleteBucketInventory() {
+    cos.deleteBucketInventory({
+        Bucket: config.Bucket,
+        Region: config.Region,
+        Id: 'inventory_test'
+    }, function(err, data) {
+        logger.log(err || JSON.stringify(data));
+    });
+}
+
+function listBucketInventory() {
+    cos.listBucketInventory({
+        Bucket: config.Bucket,
+        Region: config.Region
+    }, function(err, data) {
+        logger.log(err || JSON.stringify(data));
+    });
+}
+
 function putObject() {
     // 创建测试文件
     var filename = '1mb.zip';
@@ -1155,7 +1325,8 @@ function deleteFolder() {
         };
         next();
     };
-    _deleteFolder({
+    _delet
+    eFolder({
         Bucket: config.Bucket,
         Region: config.Region,
         Prefix: 'folder/', // 要列出的目录前缀
@@ -1190,12 +1361,24 @@ function deleteFolder() {
         'putBucketVersioning',
         'getBucketVersioning',
         'listObjectVersions',
-        'putBucketReplication',
         'getBucketReplication',
+        'putBucketReplication',
         'deleteBucketReplication',
         'putBucketWebsite',
         'getBucketWebsite',
         'deleteBucketWebsite',
+        'putBucketReferer',
+        'getBucketReferer',
+        'putBucketDomain',
+        'getBucketDomain',
+        'deleteBucketDomain',
+        'putBucketLogging',
+        'getBucketLogging',
+        'deleteBucketLogging',
+        'putBucketInventory',
+        'getBucketInventory',
+        'deleteBucketInventory',
+        'listBucketInventory',
         'deleteBucket',
         'putObjectCopy',
         'getObject',
