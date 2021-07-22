212afb51e5b283ec00af710d4edfc00eb104941c
Randy <m17607475471@163.com>
2021-07-22 14:06:05 +0800

    feat: add prettier to format code

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "none",
+  bracketSpacing: true
+};
