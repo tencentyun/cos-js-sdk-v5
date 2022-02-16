ac8c0e2f673a150f44cdeb904eeae0b482b6a4f1
chrisftian <chrisftian@tencent.com>
2022-02-16 16:20:17 +0800

    upd

--- dist/cos-js-sdk-v5.js
+++ dist/cos-js-sdk-v5.js
@@ -2429,7 +2429,7 @@ var defaultOptions = {
     CopySliceSize: 1024 * 1024 * 10,
     MaxPartNumber: 10000,
     ProgressInterval: 1000,
-    Domain: '',
+    Domain: '', // 优先级低于EndPoint
     ServiceDomain: '',
     Protocol: '',
     CompatibilityMode: false,
@@ -2442,7 +2442,10 @@ var defaultOptions = {
     UploadQueueSize: 10000,
     UploadAddMetaMd5: false,
     UploadIdCacheLimit: 50,
-    UseAccelerate: false
+    UseAccelerate: false, // 使用全球加速
+    EnableOldDomain: false, // 是否默认使用原myqcloud.com域名，默认不使用
+    EnableInternalDomain: false, // 是否使用内网域名，js默认使用外网域名
+    Endpoint: '' // 指定endPoint,比如cos.ap-guangzhou.tencentcos.cn
 };
 
 // 对外暴露的类
@@ -8006,26 +8009,39 @@ function getUrl(params) {
     var longBucket = params.bucket || '';
     var shortBucket = longBucket.substr(0, longBucket.lastIndexOf('-'));
     var appId = longBucket.substr(longBucket.lastIndexOf('-') + 1);
+    var endpoint = params.Endpoint || '';
     var domain = params.domain;
     var object = params.object;
-    if (typeof domain === 'function') {
-        domain = domain({ Bucket: longBucket, Region: region });
+    var paramsProtocol = params.protocol;
+
+    // Protocol兼容不带冒号
+    if (paramsProtocol === 'http' || paramsProtocol === 'https') {
+        paramsProtocol += ':';
     }
-    var protocol = params.protocol || (util.isBrowser && location.protocol === 'http:' ? 'http:' : 'https:');
-    if (!domain) {
-        if (['cn-south', 'cn-south-2', 'cn-north', 'cn-east', 'cn-southwest', 'sg'].indexOf(region) > -1) {
-            domain = '{Region}.myqcloud.com';
-        } else {
-            domain = 'cos.{Region}.myqcloud.com';
+    var protocol = paramsProtocol || (util.isBrowser && location.protocol === 'http:' ? 'http:' : 'https:');
+    if (endpoint) {
+        domain = protocol + '//' + longBucket + '.' + endpoint;
+    } else {
+        // 使用cn域名还是老com域名
+        var ext = params.EnableOldDomain ? '.myqcloud.com' : '.tencentcos.cn';
+        if (typeof domain === 'function') {
+            domain = domain({ Bucket: longBucket, Region: region });
         }
-        if (!params.ForcePathStyle) {
-            domain = '{Bucket}.' + domain;
+        if (!domain) {
+            if (['cn-south', 'cn-south-2', 'cn-north', 'cn-east', 'cn-southwest', 'sg'].indexOf(region) > -1) {
+                domain = '{Region}' + ext;
+            } else {
+                domain = 'cos.{Region}' + ext;
+            }
+            if (!params.ForcePathStyle) {
+                domain = '{Bucket}.' + domain;
+            }
         }
-    }
-    domain = domain.replace(/\{\{AppId\}\}/ig, appId).replace(/\{\{Bucket\}\}/ig, shortBucket).replace(/\{\{Region\}\}/ig, region).replace(/\{\{.*?\}\}/ig, '');
-    domain = domain.replace(/\{AppId\}/ig, appId).replace(/\{BucketName\}/ig, shortBucket).replace(/\{Bucket\}/ig, longBucket).replace(/\{Region\}/ig, region).replace(/\{.*?\}/ig, '');
-    if (!/^[a-zA-Z]+:\/\//.test(domain)) {
-        domain = protocol + '//' + domain;
+        domain = domain.replace(/\{\{AppId\}\}/ig, appId).replace(/\{\{Bucket\}\}/ig, shortBucket).replace(/\{\{Region\}\}/ig, region).replace(/\{\{.*?\}\}/ig, '');
+        domain = domain.replace(/\{AppId\}/ig, appId).replace(/\{BucketName\}/ig, shortBucket).replace(/\{Bucket\}/ig, longBucket).replace(/\{Region\}/ig, region).replace(/\{.*?\}/ig, '');
+        if (!/^[a-zA-Z]+:\/\//.test(domain)) {
+            domain = protocol + '//' + domain;
+        }
     }
 
     // 去掉域名最后的斜杆
@@ -8366,6 +8382,8 @@ function _submitRequest(params, callback) {
         ForcePathStyle: self.options.ForcePathStyle,
         protocol: self.options.Protocol,
         domain: self.options.Domain,
+        EnableOldDomain: self.options.EnableOldDomain,
+        Endpoint: self.options.Endpoint,
         bucket: bucket,
         region: region,
         object: object
