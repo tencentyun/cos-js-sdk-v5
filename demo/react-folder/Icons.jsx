812c6b0b20de9095f62336388b68c6b790d01ddf
L-H-B-666 <110395989+L-H-B-666@users.noreply.github.com>
2025-04-16 11:00:58 +0800

    Add React Folder Demo (#224)

---
+++ demo/react-folder/Icons.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+/**文件夹 icon */
+export const FolderIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="58" height="58" viewBox="-1 -1 58 58">
+    <defs>
+      <linearGradient x1="0%" y1="100%" y2="100%" id="bna">
+        <stop stopColor="#66A7FF" stopOpacity="0" offset="0%" />
+        <stop stopColor="#66A7FF" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h56v56H0z" />
+      <path d="M5.5 7H21l8.75 8.75H51.5a1 1 0 0 1 1 1V48a1 1 0 0 1-1 1h-47a1 1 0 0 1-1-1V9a2 2 0 0 1 2-2z" fill="#99C5FF" />
+      <path fill="url(#bna)" d="M29.75 15.75L21 7H3.5v8.75z" />
+    </g>
+  </svg>
+);
+/**文件 icon */
+export const FileIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="58" height="58" viewBox="-1 -1 58 58">
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h56v56H0z" />
+      <path d="M7.5 4v48h41V15.956L36.543 4H7.5z" stroke="#E1E2E6" fill="#FFF" />
+      <path d="M28 38.5V42H15.75v-3.5H28zm12.25-7V35h-24.5v-3.5h24.5zm0-7V28h-24.5v-3.5h24.5z" fill="#59A1FF" />
+      <path fill="#DEE1E6" d="M36.75 3.5L49 15.75H36.75z" />
+    </g>
+  </svg>
+);
